ccd8ab6244fb5694b24c9382f021f1b28de7a446
Anish-Gulati-10 <theanishgulati@gmail.com>
2025-03-26 01:31:23 +0530

    initialize React project with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  
+
+  return (
+    <section className="h-screen flex justify-center items-center bg-[#F8F8F8]">
+      <h1 className="text-3xl text-blue-500">PresidentClone</h1>
+    </section>
+  )
+}
+
+export default App
