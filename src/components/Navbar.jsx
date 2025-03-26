6a37f449287bd8ebe3cf7c498994b454d095b951
Anish-Gulati-10 <theanishgulati@gmail.com>
2025-03-26 21:45:34 +0530

    add font files and Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {logo} from '../assets/index';
+
+const Navbar = () => {
+  return (
+    <nav className='md:h-[88px] h-[64px] bg-white border-b border-[#D6D6D6] px-4 py-6 md:py-7 md:px-10 flex justify-between items-center'>
+        <div className='flex items-baseline gap-3'>
+            <img src={logo} alt="Toyota Connected" className='object-contain'/>
+            <h1 className='text-sm text-[#333333] font-semibold leading-none'>大統領と話す</h1>
+        </div>
+        X
+    </nav>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'articulate-thin': ['ArticulateCF-Thin', 'sans-serif'],
+        'articulate-extralight': ['ArticulateCF-ExtraLight', 'sans-serif'],
+        'articulate-light': ['ArticulateCF-Light', 'sans-serif'],
+        'articulate-normal': ['ArticulateCF-Normal', 'sans-serif'],
+        'articulate-regular': ['ArticulateCF-Regular', 'sans-serif'],
+        'articulate-medium': ['ArticulateCF-Medium', 'sans-serif'],
+        'articulate-demibold': ['ArticulateCF-DemiBold', 'sans-serif'],
+        'articulate-bold': ['ArticulateCF-Bold', 'sans-serif'],
+        'articulate-extrabold': ['ArticulateCF-ExtraBold', 'sans-serif'],
+        'articulate-heavy': ['ArticulateCF-Heavy', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
